9715ccd79f521ce743fcbd43f4b3fb221728c502
Haziq Aiman Helmi <haziq.helmi99@gmail.com>
2020-08-20 22:03:40 +0800

    Installed CocoaPods onto Xcode project and implemented libraries SwiftyXMLParse & Alamofire. Also created and imported XML file.

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'myRecipes' do
+  pod 'SwiftyXMLParser', :git => 'https://github.com/yahoojapan/SwiftyXMLParser.git'
+  pod 'Alamofire', '~> 4.9.1'
+  pod 'Alamofire-SwiftyXMLParser'
+end
--- myRecipes/ViewController.swift
+++ myRecipes/ViewController.swift
@@ -7,11 +7,31 @@
 //
 
 import UIKit
+import SwiftyXMLParser
+import Alamofire
+
+struct Recipes: Codable {
+    let name: String
+    let description: String
+    let ingredients: String
+    let steps: String
+}
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        guard let urlPath = Bundle.main.path(forResource: "recipes", ofType: "xml") else { return }
+        let url = NSURL.fileURL(withPath: urlPath)
+        
+        Alamofire.request(url).responseData {
+            response in
+            if let data = response.data {
+                let xml = XML.parse(data)
+                print(xml.RecipeSet.Entree.Recipe[0].Title.text!)
+            }
+        }
         // Do any additional setup after loading the view.
     }
 
